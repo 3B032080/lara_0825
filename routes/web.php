da68daba70c3fc654085c4ea169a18d20ce71673
MSI\Aloi <kasper1116.kh@gmail.com>
2024-01-08 00:08:19 +0800

    用 new 的方式新增資料

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $post=new Post();
+    $post->title ='testtitle';
+    $post->content ='testcontent';
+    $post->save();
     return view('welcome');
 });
